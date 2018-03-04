914b5ca33a8a0ba554062de26516c714393ce447
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-03-04 21:29:59 +1300

    Base React project setup along side base scss and flatmate stylings

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      // Empty footer
+      <footer className="app-footer">
+      </footer>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Table,
+} from 'reactstrap';
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
